270760d93cb5d03399aa8a3b01f6645c7365dde3
hanyun <1355081829@qq.com>
2018-06-13 12:53:23 +0800

    社团列表页面

--- resources/views/home/club/index.blade.php
+++ resources/views/home/club/index.blade.php
@@ -15,7 +15,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-10">
+        <div class="col-9">
             <div class="row">
                 @for($i=0;$i<16;$i++)
                     <div class="col-3">
@@ -25,6 +25,15 @@
                                  alt="Card image">
                             <div class="card-body">
                                 <h5 class="card-title text-center">篮球协会</h5>
+                                <p class="card-text text-left small">
+                                    篮球协会的口号是目的是增加大家之间的沟通交流
+                                </p>
+                                <div class="row">
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">关注：1000</li>
+                                        <li class="list-inline-item">成员：100</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -33,22 +42,33 @@
             <div class="row justify-content-center mt-2">
                 <ul class="pagination">
                     <li class="page-item disabled"><a class="page-link" href="#">上一页</a></li>
-                    @for($i=0;$i<20;$i++)
+                    @for($i=0;$i<16;$i++)
                         <li class="page-item"><a class="page-link" href="#">{{$i+1}}</a></li>
                     @endfor
                     <li class="page-item"><a class="page-link" href="#">下一页</a></li>
                 </ul>
             </div>
         </div>
-        <div class="col-2">
-            <h4 class="text-primary">我的社团</h4>
-            <ul class="list-unstyled">
-                <li><a href="#">计算机</a></li>
-                <li><a href="#">计算机</a></li>
-                <li><a href="#">计算机</a></li>
-                <li><a href="#">计算机</a></li>
-                <li><a href="#">计算机</a></li>
-            </ul>
+        <div class="col-3">
+            <div class="card mb-2">
+                <div class="card-header">我的关注</div>
+                <ul class="list-group">
+                    <li class="list-group-item">好友：100<a href="/user/friend" class="text-info float-right small">查看</a></li>
+                    <li class="list-group-item">社团：100<a href="/user/club" class="text-info float-right small">查看</a></li>
+                    <li class="list-group-item">高校：100<a href="/user/school" class="text-info float-right small">查看</a></li>
+                </ul>
+            </div>
+            <div class="card mb-1">
+                <div class="card-header">管理的社团</div>
+                <ul class="list-group">
+                    <li class="list-group-item">计算机 <a href="/club/edit" class="text-info float-right small">管理</a></li>
+                    <li class="list-group-item">计算机 <a href="/club/edit" class="text-info float-right small">管理</a></li>
+                    <li class="list-group-item">计算机 <a href="/club/edit" class="text-info float-right small">管理</a></li>
+                    <li class="list-group-item">计算机 <a href="/club/edit" class="text-info float-right small">管理</a></li>
+                    <li class="list-group-item">计算机 <a href="/club/edit" class="text-info float-right small">管理</a></li>
+                    <li class="list-group-item">计算机 <a href="/club/edit" class="text-info float-right small">管理</a></li>
+                </ul>
+            </div>
             <a class="btn btn-primary text-white" href="/club/add">新建社团</a>
         </div>
     </div>
